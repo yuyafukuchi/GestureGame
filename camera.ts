b8f53996807409d37ff337727028ffb928107955
近藤大翔 <taisho90@icloud.com>
2020-05-24 10:53:32 +0900

    コメント修正

--- camera.ts
+++ camera.ts
@@ -488,7 +488,6 @@ function detectPoseInRealTime(video: HTMLVideoElement, net: posenet.PoseNet) {
           drawBoundingBox(keypoints, ctx);
         }
 
-        // 右目が左によると、色が赤に変わる
         ctx.beginPath();
         ctx.arc(120, 120, 100, 0, 2 * Math.PI);
         let circle = {
@@ -496,6 +495,8 @@ function detectPoseInRealTime(video: HTMLVideoElement, net: posenet.PoseNet) {
           y: 120,
           r: 100,
         };
+        // 右目が円内に入ると、色が赤に変わる
+        // 検知の信頼性を担保するために閾値を0.8で指定
         if (keypoints[2].score > 0.8
           && isInCircle(circle, keypoints[2].position)
         ) {
